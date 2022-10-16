b907a353acb32bb37f8f377712c8b4272f505977
Philippe Rouquier <bonfire-app@wanadoo.fr>
2022-10-16 19:00:12 +0200

    Use libadwaita new widget entryrow for managing the utterances associated with a shortcut

--- engine/sttshortcutdialog.py
+++ engine/sttshortcutdialog.py
@@ -21,8 +21,9 @@ import logging
 import gi
 
 gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
 
-from gi.repository import Gtk, Adw
+from gi.repository import GLib, Gtk, Adw
 
 from sttutterancerow import STTUtteranceRow
 from sttshortcutrow import STTShortcutRow
@@ -36,7 +37,6 @@ class STTShortcutDialog(Gtk.Dialog):
     add_button=Gtk.Template.Child()
     apply_button=Gtk.Template.Child()
     add_utterance_button=Gtk.Template.Child()
-    apply_utterance_button=Gtk.Template.Child()
     previous_button=Gtk.Template.Child()
     cancel_button=Gtk.Template.Child()
 
@@ -47,7 +47,6 @@ class STTShortcutDialog(Gtk.Dialog):
     button_stack_end=Gtk.Template.Child()
 
     utterance_list=Gtk.Template.Child()
-    utterance_entry=Gtk.Template.Child()
 
     text_label=Gtk.Template.Child()
 
@@ -63,16 +62,13 @@ class STTShortcutDialog(Gtk.Dialog):
         self._row=row
         self._added_temp={}
         self._removed_temp={}
+        self._changes=0
         self._rows_list=[]
-        self._activated_row=None
 
         self.button_stack_start.set_visible_child(self.cancel_button)
 
-        buffer = self.utterance_entry.get_buffer()
-        buffer.connect("notify::text", self.utterance_text_changed)
-
         buffer=self.text_view.get_buffer()
-        buffer.connect("notify::text", self.value_text_changed)
+        buffer.connect("notify::text", self._value_text_changed)
 
         # I don't know why but we need this ??? Otherwise it does not show up.
         self.set_titlebar(self.header)
@@ -100,7 +96,7 @@ class STTShortcutDialog(Gtk.Dialog):
         buffer=self.description_entry.get_buffer()
         if row.description not in (None, ""):
             buffer.set_text(row.description,-1)
-        buffer.connect("notify::text", self.description_changed)
+        buffer.connect("notify::text", self._description_changed)
 
         if isinstance(row.value, list):
             # For diacritics
@@ -127,40 +123,134 @@ class STTShortcutDialog(Gtk.Dialog):
         unique_utterances=list(set(row.utterances)|set(row._extra_utterances))
         for utterance in unique_utterances:
             utterance_row=STTUtteranceRow(text=utterance, editable=bool(utterance not in row.utterances))
-            self.utterance_list.add(utterance_row)
-            utterance_row.connect("activated", self.row_activated_cb)
-            utterance_row.connect("delete", self.row_deleted_cb)
-            self._rows_list.append(utterance_row)
+            self._add_utterance_row(utterance_row)
 
-    def row_activated_cb(self, row):
-        self._activated_row=row
-        buffer=self.utterance_entry.get_buffer()
-        buffer.set_text(row.text, -1)
+    def _delete_utterance(self, utterance):
+        num_rows=self._added_temp.get(utterance, 0)
+        num_rows -= 1
 
-        self.contents.set_visible_child_name("utterance")
-        self.button_stack_start.set_visible_child(self.previous_button)
-        self.button_stack_end.set_visible_child(self.apply_utterance_button)
-        self.set_default_widget(self.apply_utterance_button)
+        if num_rows == 0:
+            self._added_temp.pop(utterance)
+
+            parent = self.get_transient_for()
+            if parent._utterances_dict.get(utterance, False) is False:
+                self._changes -= 1
+        elif num_rows == -1:
+            self._removed_temp[utterance]=True
+            self._changes += 1
+        else:
+            self._added_temp[utterance]=num_rows
+
+    def _add_utterance(self, utterance):
+        if self._removed_temp.get(utterance) == True:
+            self._removed_temp.pop(utterance)
+            self._changes -= 1
+        else:
+            num_rows=self._added_temp.get(utterance, 0)
+            num_rows+=1
+            self._added_temp[utterance]=num_rows
+
+            parent = self.get_transient_for()
+            if num_rows == 1 and \
+               parent._utterances_dict.get(utterance, False) is False:
+               self._changes += 1
+
+    def _check_utterance_existence(self, utterance_row, utterance):
+        if utterance in ["", None] :
+            return False
+
+        parent = self.get_transient_for()
+        num_rows=self._added_temp.get(utterance, 0)
+        if parent._utterances_dict.get(utterance, False) == True and self._removed_temp.get(utterance, False) == False:
+            num_rows += 1
+
+        if num_rows != 0:
+            if utterance_row is None:
+                return True
+
+            return bool(num_rows == 1 and utterance != utterance_row.text)
+
+        return False
+
+    def _update_focus(self):
+        if self._row == None:
+            button=self.add_button
+        else:
+            button=self.apply_button
+
+        if button.get_sensitive() == False:
+            self.cancel_button.grab_focus()
+            self.set_default_widget(self.cancel_button)
+        else:
+            button.grab_focus()
+            self.set_default_widget(button)
+
+    def _remove_row_real(self, utterance_row):
+        self.utterance_list.remove(utterance_row)
+
+    def _remove_utterance_row(self, utterance_row):
+        # Make sure it cannot be removed twice
+        if utterance_row not in self._rows_list:
+            return
+
+        utterance_row.disconnect_by_func(self.utterance_row_text_changed_cb)
+        utterance_row.disconnect_by_func(self.utterance_row_activated_cb)
+        utterance_row.disconnect_by_func(self.utterance_text_changed)
+        utterance_row.disconnect_by_func(self.row_deleted_cb)
+        self._rows_list.remove(utterance_row)
+
+        # Unfortunately, this triggers a warning message but it would help
+        # make sure the row cannot be changed anymore.
+        # utterance_row.set_sensitive(False)
+
+        # This is the only way to avoid an infinite loop if we remove the row
+        # after it receives a "leave" signal from the event controller
+        GLib.idle_add(self._remove_row_real, utterance_row)
+
+    def delete_row(self, utterance_row):
+        if utterance_row.text not in [None,""]:
+            self._delete_utterance(utterance_row.text)
+
+        self._remove_utterance_row(utterance_row)
+        self._update_add_apply_buttons_state()
+        self._update_focus()
+
+    def row_deleted_cb(self, utterance_row):
+        self.delete_row(utterance_row)
+
+    def utterance_row_text_changed_cb(self, utterance_row, old_text, new_text):
+        # If old_text is empty then it is a new row
+        if old_text not in [None,""]:
+            self._delete_utterance(old_text)
+
+        # If new_text is empty, delete row
+        if new_text in [None,""]:
+            self._remove_utterance_row(utterance_row)
+        elif self._check_utterance_existence(None, new_text):
+            self._remove_utterance_row(utterance_row)
+        else:
+            self._add_utterance(new_text)
+        self._update_add_apply_buttons_state()
+
+    def utterance_row_activated_cb(self, utterance_row):
+        self._update_focus()
+
+    def _add_utterance_row(self, utterance_row):
+        self.utterance_list.add(utterance_row)
+        utterance_row.connect("text-changed", self.utterance_row_text_changed_cb)
+        utterance_row.connect("entry-activated", self.utterance_row_activated_cb)
+        utterance_row.connect("changed", self.utterance_text_changed)
+        utterance_row.connect("delete", self.row_deleted_cb)
+        self._rows_list.append(utterance_row)
 
     @Gtk.Template.Callback()
     def new_utterance_button_clicked_cb(self, button):
-        self._activated_row=None
-        buffer=self.utterance_entry.get_buffer()
-        buffer.set_text("", -1)
-
-        self.utterance_entry.set_placeholder_text("Type your utterance")
-
-        self.contents.set_visible_child_name("utterance")
-        self.button_stack_start.set_visible_child(self.previous_button)
-        self.button_stack_end.set_visible_child(self.add_utterance_button)
-        self.set_default_widget(self.add_utterance_button)
-
-        self.add_utterance_button.set_sensitive(False)
+        utterance_row=STTUtteranceRow()
+        self._add_utterance_row(utterance_row)
+        utterance_row.grab_focus()
 
     @Gtk.Template.Callback()
     def new_alternative_utterances_button_clicked_cb(self, button):
-        self._activated_row=None
-
         self.contents.set_visible_child_name("utterance_recognition")
         self.button_stack_start.set_visible_child(self.previous_button)
         self.button_stack_end.set_visible_child(self.add_utterance_button)
@@ -174,8 +264,6 @@ class STTShortcutDialog(Gtk.Dialog):
         self.recognize_toggle.grab_focus()
 
     def display_global_page(self):
-        self._activated_row=None
-
         self.contents.set_visible_child_name("global")
         self.button_stack_start.set_visible_child(self.cancel_button)
 
@@ -190,33 +278,34 @@ class STTShortcutDialog(Gtk.Dialog):
             self.alternatives_list.remove(row)
 
         self._alternative_rows=[]
-
         self._stop_recognition()
 
     @Gtk.Template.Callback()
     def previous_button_clicked_cb(self, button):
         self.display_global_page()
 
-    def _add_utterance_row(self, utterance):
+    def _add_alternative_utterance(self, utterance):
         # Utterance can't be in _added_temp (we checked it while it was typed)
+        # AND the row has no previous value (contrary to what can happen in
+        # apply_utterance_row_button_clicked_cb())
         if self._removed_temp.get(utterance) == True:
             self._removed_temp.pop(utterance)
         else:
             self._added_temp[utterance]=True
 
         utterance_row=STTUtteranceRow(text=utterance, editable=bool(utterance not in self.utterances))
-        self.utterance_list.add(utterance_row)
-        utterance_row.connect("delete", self.row_deleted_cb)
-        utterance_row.connect("activated", self.row_activated_cb)
-        self._rows_list.append(utterance_row)
+        self._add_utterance_row(utterance_row)
 
-    def _add_utterance_from_listbox(self):
+    # Used when the row is added from a list of results returned after recognition
+    @Gtk.Template.Callback()
+    def add_alternative_utterance_button_clicked_cb(self, button):
         for row in self._alternative_rows:
-            if row.get_icon_name() != None and row.get_icon_name() != "":
+            # Check if the row is selected by checking the presence of an icon
+            if row.get_icon_name() not in [None,""]:
                 self.alternatives_list.remove(row)
                 continue
 
-            self._add_utterance_row(row.get_title())
+            self._add_alternative_utterance(row.get_title())
             self.alternatives_list.remove(row)
 
         self._alternative_rows=[]
@@ -224,122 +313,14 @@ class STTShortcutDialog(Gtk.Dialog):
         self._update_add_apply_buttons_state()
         self.display_global_page()
 
-    def _add_utterance_from_entry(self):
-        buffer=self.utterance_entry.get_buffer()
-        utterance=buffer.get_text()
+    def utterance_text_changed(self, utterance_row):
+        if utterance_row.editing == False:
+            return
 
-        self._add_utterance_row(utterance)
-        self._update_add_apply_buttons_state()
+        utterance = utterance_row.get_text()
+        show_warning = self._check_utterance_existence(utterance_row, utterance)
 
-        self.display_global_page()
-
-    @Gtk.Template.Callback()
-    def add_utterance_button_clicked_cb(self, button):
-        if self.contents.get_visible_child_name() == "utterance":
-            self._add_utterance_from_entry()
-        else:
-            self._add_utterance_from_listbox()
-
-    @Gtk.Template.Callback()
-    def apply_utterance_button_clicked_cb(self, button):
-        # If the button has been clicked, the text HAS been modified
-        buffer=self.utterance_entry.get_buffer()
-        utterance=buffer.get_text()
-
-        # Utterance can't be in _added_temp (we checked it while it was typed)
-        if self._removed_temp.get(utterance) == True:
-            self._removed_temp.pop(utterance)
-        else:
-            self._added_temp[utterance]=True
-
-        if self._added_temp.get(self._activated_row.text) == True:
-            self._added_temp.pop(self._activated_row.text)
-        else:
-            self._removed_temp[self._activated_row.text]=True
-
-        self._activated_row.text=utterance
-        self._activated_row.update()
-
-        self.display_global_page()
-
-    def row_deleted_cb(self, row):
-        if self._added_temp.get(row.text) == True:
-            self._added_temp.pop(row.text)
-        else:
-            self._removed_temp[row.text]=True
-
-        self.utterance_list.remove(row)
-        self._rows_list.remove(row)
-        self._update_add_apply_buttons_state()
-
-    def apply_to_row(self):
-        # We don't set value unless it's editable
-        if self._row.editable:
-            LOG_MSG.debug("row is editable")
-            # we don't need to be careful about diacritics here as value
-            # won't be changed.
-            buffer=self.text_view.get_buffer()
-            self._row.value=buffer.get_text(buffer.get_start_iter(), buffer.get_end_iter(), False)
-            LOG_MSG.debug("row value %s", self._row.value)
-
-        buffer=self.description_entry.get_buffer()
-        self._row.description=buffer.get_text()
-
-        extra=[]
-        for row in self._rows_list:
-            # All utterances cannot be removed nor changed
-            if row.text in self._row.utterances:
-                continue
-
-            extra.append(row.text)
-
-        self._row.set_extra_utterances(extra)
-        return (self._added_temp, self._removed_temp)
-
-    def get_new_row(self):
-        buffer=self.text_view.get_buffer()
-        value=buffer.get_text(buffer.get_start_iter(), buffer.get_end_iter(), False)
-
-        buffer=self.description_entry.get_buffer()
-        description=buffer.get_text()
-
-        extra=[]
-        for row in self._rows_list:
-            extra.append(row.text)
-
-        return STTShortcutRow(value=value,
-                              extra_utterances=extra,
-                              description=description,
-                              editable=True)
-
-    def _check_utterance_existence(self, utterance):
-        parent = self.get_transient_for()
-        if utterance == "" :
-            sensitive=False
-            show_warning=False
-        elif parent._utterances_dict.get(utterance, False) == True or self._added_temp.get(utterance) == True:
-            sensitive=False
-            show_warning=bool(self._activated_row == None or utterance != self._activated_row.text)
-        else:
-            sensitive=True
-            show_warning=False
-
-        return (sensitive, show_warning)
-
-    def utterance_text_changed(self, buffer, text):
-        utterance = buffer.get_text()
-        (sensitive, show_warning) = self._check_utterance_existence(utterance)
-
-        if show_warning == True:
-            self.utterance_entry.set_icon_from_icon_name(Gtk.EntryIconPosition.SECONDARY, "dialog-warning-symbolic")
-            self.utterance_entry.set_icon_tooltip_text(Gtk.EntryIconPosition.SECONDARY, "The utterance already exists")
-        else:
-            self.utterance_entry.set_icon_from_icon_name(Gtk.EntryIconPosition.SECONDARY, None)
-            self.utterance_entry.set_icon_tooltip_text(Gtk.EntryIconPosition.SECONDARY, None)
-
-        button=self.button_stack_end.get_visible_child()
-        self.set_default_widget(button)
-        button.set_sensitive(sensitive)
+        utterance_row.valid_image.set_visible(show_warning)
 
     def _update_add_apply_buttons_state(self):
         buffer=self.text_view.get_buffer()
@@ -373,20 +354,24 @@ class STTShortcutDialog(Gtk.Dialog):
             # Same value, same description
             sensitive=True
             LOG_MSG.info("description has changed")
-        elif len(self._removed_temp) != 0 or len (self._added_temp) != 0:
+        elif self._changes != 0:
             sensitive=True
             LOG_MSG.info("new or removed utterance")
         else:
             LOG_MSG.info("nothing has changed")
             sensitive=False
 
-        self.add_button.set_sensitive(sensitive)
-        self.apply_button.set_sensitive(sensitive)
+        if self._row == None:
+            button=self.add_button
+        else:
+            button=self.apply_button
 
-    def value_text_changed(self, buffer, text):
+        button.set_sensitive(sensitive)
+
+    def _value_text_changed(self, buffer, text):
         self._update_add_apply_buttons_state()
 
-    def description_changed(self, buffer, text):
+    def _description_changed(self, buffer, text):
         self._update_add_apply_buttons_state()
 
     def _stop_recognition(self):
@@ -396,7 +381,7 @@ class STTShortcutDialog(Gtk.Dialog):
 
         self._engine.stop()
 
-    def _new_utterance_row_delete_clicked_cb(self, button):
+    def _new_alternative_utterance_row_delete_clicked_cb(self, button):
         row=button.get_parent()
         while not isinstance(row, Adw.ActionRow):
             row=row.get_parent()
@@ -429,15 +414,14 @@ class STTShortcutDialog(Gtk.Dialog):
             style_context=delete_button.get_style_context()
             style_context.add_class("circular")
             style_context.add_class("flat")
-            delete_button.connect("clicked", self._new_utterance_row_delete_clicked_cb)
+            delete_button.connect("clicked", self._new_alternative_utterance_row_delete_clicked_cb)
             row.add_suffix(delete_button)
 
             self.alternatives_list.append(row)
             self._alternative_rows.append(row)
 
             # Check if utterance exists
-            (sensitive, utterance_exists)=self._check_utterance_existence(utterance)
-            if utterance_exists == False:
+            if self._check_utterance_existence(None, utterance) == False:
                 num_valid_utterances += 1
             else:
                 row.set_icon_name("dialog-warning-symbolic")
@@ -483,3 +467,45 @@ class STTShortcutDialog(Gtk.Dialog):
     def _model_changed_cb(self, engine):
         self._update_recognize_button()
         self.recognize_toggle.set_sensitive(self._engine.has_model())
+
+    # Called by STTConfigDialog to update a STTShortcutRow
+    def apply_to_row(self):
+        # We don't set value unless it's editable
+        if self._row.editable:
+            LOG_MSG.debug("row is editable")
+            # we don't need to be careful about diacritics here as value
+            # won't be changed.
+            buffer=self.text_view.get_buffer()
+            self._row.value=buffer.get_text(buffer.get_start_iter(), buffer.get_end_iter(), False)
+            LOG_MSG.debug("row value %s", self._row.value)
+
+        buffer=self.description_entry.get_buffer()
+        self._row.description=buffer.get_text()
+
+        extra=[]
+        for row in self._rows_list:
+            # All utterances cannot be removed nor changed
+            if row.text in self._row.utterances:
+                continue
+
+            extra.append(row.text)
+
+        self._row.set_extra_utterances(extra)
+        return (self._added_temp, self._removed_temp)
+
+    # Called by STTConfigDialog to get a new STTShortcutRow
+    def get_new_row(self):
+        buffer=self.text_view.get_buffer()
+        value=buffer.get_text(buffer.get_start_iter(), buffer.get_end_iter(), False)
+
+        buffer=self.description_entry.get_buffer()
+        description=buffer.get_text()
+
+        extra=[]
+        for row in self._rows_list:
+            extra.append(row.text)
+
+        return STTShortcutRow(value=value,
+                              extra_utterances=extra,
+                              description=description,
+                              editable=True)
--- engine/sttutterancerow.py
+++ engine/sttutterancerow.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from gettext import gettext as _
 
 import gi
 
@@ -26,28 +27,67 @@ from gi.repository import GObject
 from gi.repository import Adw
 
 @Gtk.Template(resource_path="/org/freedesktop/ibus/engine/stt/config/sttutterancerow.ui")
-class STTUtteranceRow (Adw.ActionRow):
+class STTUtteranceRow (Adw.EntryRow):
     __gtype_name__="STTUtteranceRow"
 
     __gsignals__ = {
-        "delete": (GObject.SIGNAL_RUN_FIRST, None,())
+        "delete": (GObject.SIGNAL_RUN_FIRST, None,()),
+        "text-changed": (GObject.SIGNAL_RUN_FIRST, None,(str,str)),
     }
 
     delete_button=Gtk.Template.Child()
+    valid_image=Gtk.Template.Child()
 
     def __init__(self, text="", editable=True):
         super().__init__()
-        self.set_title(text)
 
         self.editable=editable
         self.text=text
+        self.set_title(self.text)
         if editable == False:
             self.delete_button.set_visible(False)
             self.set_sensitive(False)
+
+        self.editing=False
+        self._focus_controller=Gtk.EventControllerFocus.new()
+        self.add_controller(self._focus_controller)
+        self._focus_controller.connect_after("leave", self.leave_event)
+        self._focus_controller.connect("enter", self.enter_event)
+
+        self.connect("entry-activated", self.activated_cb)
 
     @Gtk.Template.Callback()
     def delete_button_clicked_cb(self, button):
         self.emit("delete")
 
-    def update(self):
-        self.set_title(self.text.strip())
+    def _validate_utterance(self):
+        if self.editing == False:
+            return
+
+        self.editing=False
+        # default handler, used to update the value of self.text
+        text=self.get_text()
+        if self.text != text or text in ["", None]:
+            self.emit("text-changed", self.text, text)
+            self.text=text
+
+        self.set_title(text)
+
+        # Reset to nothing
+        self.set_text("")
+
+    # do_activated does not seem possible as there is no virtual method
+    def activated_cb(self, row):
+        self._validate_utterance()
+
+    def enter_event(self, controller):
+        self.editing=True
+        self.set_text(self.text)
+        self.set_title("")
+        if self.text in [None,""]:
+            delegate=self.get_delegate()
+            delegate.set_placeholder_text(_("Type your utterance"))
+            self.valid_image.set_visible(True)
+
+    def leave_event(self, controller):
+        self._validate_utterance()
